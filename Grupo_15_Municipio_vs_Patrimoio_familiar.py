1860f89fcf02631d41a265ed4f65cf263e32ba58
DanteFarfan <129178199+DanteFarfan@users.noreply.github.com>
2023-05-08 13:15:20 -0400

    Update Grupo_15_Municipio_vs_Patrimoio_familiar.py

--- Grupo_15_Municipio_vs_Patrimoio_familiar.py
+++ Grupo_15_Municipio_vs_Patrimoio_familiar.py
@@ -1,2 +1,35 @@
 #Grupo 15: Municipio vs Patrimonio Familiar 
 # Dante Farfan - Tomás Teihuel - Joaquin Lespai - Andres Vera - Ricardo Angulo 
+import sys
+
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QMainWindow, \
+    QPushButton, \
+    QDialog, QDialogButtonBox, QMessageBox, QGridLayout
+from PyQt6.QtCore import QSize
+from PyQt6.QtGui import QColor, QFont, QMouseEvent
+from random import randint
+
+
+class MiniWindow(QDialog):
+    def __init__(self, a0: QMainWindow):
+        super().__init__()
+        self.setWindowTitle("MiniWindow")
+        self.setMinimumSize(200, 100)
+        self.topParent = a0
+        self.index = 0
+        QBtn = QDialogButtonBox.StandardButton.Ok
+        self.button = QDialogButtonBox(QBtn)
+        self.button.clicked.connect(self.setData)
+        self.button.accepted.connect(self.accept)
+        self.main_layout = QVBoxLayout()
+        self.layout = QGridLayout()
+        self.entry0 = QLineEdit("0")
+        self.entry1 = QLineEdit("0")
+        self.entry2 = QLineEdit("0")
+        self.entry3 = QLineEdit("0")
+        self.layout.addWidget(QLabel("Atributo 1"), 0, 0)
+        self.layout.addWidget(self.entry0, 0, 1)
+        self.main_layout.addWidget(QLabel("Ingresa los datos:"))
+        self.main_layout.addLayout(self.layout)
+        self.main_layout.addWidget(self.button)
+        self.setLayout(self.main_layout)
